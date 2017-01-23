00021c349112cf8295f8f103e7d6a00a98ee06e8
Alan Fischer <alan@quarq.com>
2017-01-23 15:40:20 -0700

    Fixed python 2 support

--- pyitachip2ir.py
+++ pyitachip2ir.py
@@ -3,7 +3,9 @@ Control an itach ip2ir gateway using libitachip2ir
 """
 from ctypes import *
 import os
+import sys
 
+libitachip2ir = None
 for ext in ['so','dylib','dll']:
     for pre in ['lib','']:
         try:
@@ -11,9 +13,7 @@ for ext in ['so','dylib','dll']:
             break
         except OSError:
             pass
-try:
-    libitachip2ir
-except NameError:
+if libitachip2ir is None:
     raise OSError("Unable to find itachip2ir library")
 
 libitachip2ir.ITachIP2IR_new.argtypes = [c_char_p, c_char_p, c_int]
@@ -27,9 +27,16 @@ libitachip2ir.ITachIP2IR_addDevice.restype = c_bool
 libitachip2ir.ITachIP2IR_send.argtypes = [c_void_p, c_char_p, c_char_p, c_int]
 libitachip2ir.ITachIP2IR_send.restype = c_bool
 
+if sys.version_info >= (3, 0):
+    def tochar(data):
+        return bytes(data,'utf-8')
+else:
+    def tochar(data):
+        return str(data)
+
 class ITachIP2IR(object):
     def __init__(self, mac, ip, port):
-        self.itachip2ir = libitachip2ir.ITachIP2IR_new(bytes(mac,'utf-8'), bytes(ip,'utf-8'), port)
+        self.itachip2ir = libitachip2ir.ITachIP2IR_new(tochar(mac), tochar(ip), port)
 
     def __del__(self):
         libitachip2ir.ITachIP2IR_delete(self.itachip2ir)
@@ -38,7 +45,7 @@ class ITachIP2IR(object):
         libitachip2ir.ITachIP2IR_update(self.itachip2ir)
 
     def addDevice(self, name, modaddr, connaddr, cmddata):
-        return libitachip2ir.ITachIP2IR_addDevice(self.itachip2ir, bytes(name,'utf-8'), modaddr, connaddr, bytes(cmddata,'utf-8'))
+        return libitachip2ir.ITachIP2IR_addDevice(self.itachip2ir, tochar(name), modaddr, connaddr, tochar(cmddata))
 
     def send(self, device, command, count):
-        libitachip2ir.ITachIP2IR_send(self.itachip2ir, bytes(device,'utf-8'), bytes(command,'utf-8'), count)
+        libitachip2ir.ITachIP2IR_send(self.itachip2ir, tochar(device), tochar(command), count)
